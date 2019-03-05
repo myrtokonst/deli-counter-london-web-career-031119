9be95a48fa7d0c9e705c2b73d56b5f60e2a6bb9b
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-05 15:32:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,10 @@ def line(other_line)
  if other_line==[]
    puts "The line is currently empty."
  else 
+   phrase = "The line is currently:"
+   other_line.each_with_index do |n,i|
+     phrase.push("#{i+1}. #{n}.")
+   end 
+   puts phrase
  end
 end
