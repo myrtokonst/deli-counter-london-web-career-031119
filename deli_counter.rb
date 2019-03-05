bb7013f0f3dc791455c06438c268d51ead20b027
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-05 15:51:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,6 @@ def now_serving(katz_deli)
 if katz_deli==[]
   puts "There is nobody waiting to be served!"
 else
-  katz_deli.pop()
-  puts "Currently serving #{katz_deli.pop()}." 
+  puts "Currently serving #{katz_deli.shift()}." 
 end 
 end 
