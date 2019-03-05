8cabfb6d23ced2aabd227f4fc5729419eec86216
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-05 15:34:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(other_line)
  else 
    phrase = "The line is currently: "
    other_line.each_with_index do |n,i|
-     phrase<<"#{i+1}. #{n}."
+     phrase<<"#{i+1}. #{n} "
    end 
    puts phrase
  end
